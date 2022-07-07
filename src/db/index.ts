4db419e7e7815157be2dedaa84f1fe91c97acd4d
thousandcows <milcamboo@gmail.com>
2022-07-07 23:53:32 +0900

    Style 🎨: 코드 구조 변경 - model을 index.ts에서 일괄적으로 export하기로 합의 - Chat model export 추가

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import ChatSchema from './schemas/chat-schema';
 
 const DB_URL = process.env.MONGODB_URL
   || 'MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.ts 파일을 확인해 주세요. \n.env 파일도 필요합니다.\n';
@@ -8,3 +9,5 @@ const db = mongoose.connection;
 
 db.on('connected', () => console.log(`정상적으로 MongoDB 서버에 연결되었습니다.  ${DB_URL}`));
 db.on('error', (error) => console.error(`\nMongoDB 연결에 실패하였습니다...\n${DB_URL}\n${error}`));
+
+export const Chat = mongoose.model('chats', ChatSchema);
