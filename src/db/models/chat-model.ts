bd348899458bc06169e5556406dd3f00770ed929
thousandcows <milcamboo@gmail.com>
2022-07-07 23:54:12 +0900

    FEAT ✨: 새 메시지 db 추가 기능
    - 사용자가 메시지를 전송하면 시간 순서대로 DB에 저장된다
    
    # issue: 1

--- src/db/models/chat-model.ts
+++ src/db/models/chat-model.ts
@@ -1,16 +1,36 @@
-import { model } from 'mongoose';
-import ChatSchema from '../schemas/chat-schema';
+/* eslint-disable max-len */
+import { Types, Document } from 'mongoose';
+import { Chat } from '../index';
 
-const Chat = model('chats', ChatSchema);
+interface ChatInfo {
+  roomType: string;
+  username: string;
+  message: string;
+  time?: string;
+  image: string;
+}
+interface ChatData extends Document<Types.ObjectId> {
+  roomType: string;
+  username: string;
+  message: string;
+  time?: string;
+  image: string;
+}
+export class ChatModel {
+  // 1. 새 메시지 추가
+  async addChat(chat: ChatInfo): Promise<ChatData> {
+    const result = await Chat.create(chat);
+    return result;
+  }
 
-class ChatModel {
-  // 1. 전체 메시지 조회
-  async findAll() {
+  // 2. 전체 메시지 조회
+  async findAllChats(): Promise<ChatData[]> {
     const chatList = await Chat.find({});
     return chatList;
   }
-  // 2. 메시지 조회 페이지네이션
-  async getPaginatedChats(page: number, perPage: number) {
+
+  // 3. 메시지 조회 페이지네이션
+  async getPaginatedChats(page: number, perPage: number): Promise<[charList: ChatData[], totalPage: number]> {
     const [total, chatList] = await Promise.all([
       Chat.countDocuments({}),
       Chat
@@ -24,6 +44,4 @@ class ChatModel {
   }
 }
 
-const chatModel = new ChatModel();
-
-export default chatModel;
+export const chatModel = new ChatModel();
