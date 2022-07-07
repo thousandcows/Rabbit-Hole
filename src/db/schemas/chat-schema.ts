0ff8f4e9373e9c898ec76c15ee959e323cffc6f4
thousandcows <milcamboo@gmail.com>
2022-07-07 17:49:22 +0900

    FEAT ✨: chat 스키마 정의 - 채팅 기본 스키마 정의 완료 # issue: 1

---
+++ src/db/schemas/chat-schema.ts
@@ -0,0 +1,32 @@
+import { Schema } from 'mongoose';
+
+const ChatSchema = new Schema(
+  {
+    roomType: {
+      type: String,
+      required: true,
+      default: 'main',
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      require: true,
+    },
+    image: {
+      type: String,
+    },
+  },
+  {
+    collection: 'chats',
+    timestamps: true,
+  },
+);
+
+export default ChatSchema;
