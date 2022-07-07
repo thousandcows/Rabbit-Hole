8e16239208154e0ce7729b65e09b4afa6066558a
thousandcows <milcamboo@gmail.com>
2022-07-07 17:23:15 +0900

    FEAT ✨: 라우팅: 기본 채팅 기능 구현 테스트

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,11 @@ app.use(express.urlencoded({ extended: false }));
 
 const { PORT } = process.env;
 
+app.get('/', (req: any, res: any) => {
+  // eslint-disable-next-line no-path-concat, prefer-template
+  res.sendFile(__dirname + '/index.html');
+});
+
 app.use('/api', apiRouter);
 
 const server = app.listen(PORT, () => console.log(`server is running ${PORT}`));
