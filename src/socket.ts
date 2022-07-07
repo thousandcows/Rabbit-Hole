2d5c04a89c9cb36ca4eda988acc258f5379beaad
thousandcows <milcamboo@gmail.com>
2022-07-07 23:49:16 +0900

    FEAT ✨: 새 메시지 db 추가 기능
    - 사용자가 메시지를 전송하면 시간 순서대로 DB에 저장된다
    
    # issue: 1

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,6 @@
 import * as http from 'http';
 import { Server } from 'socket.io';
+import { chatService } from './services/chat-service';
 
 function webSocket(server: http.Server) {
   const io = new Server(server, {
@@ -15,7 +16,7 @@ function webSocket(server: http.Server) {
     socket.on('newUser', (newUser: string) => {
       // user에게 채팅방에 입장했음을 알림
       socket.emit('updateForNewUser', `${newUser}(나)님이 입장했습니다.`);
-      // 전체 접속자에게 user가 채팅방에 입장했음을 알림 
+      // 전체 접속자에게 user가 채팅방에 입장했음을 알림
       socket.broadcast.emit('updateForEveryone', `${newUser}가 도착했습니다.`);
     });
     // 메시지 전송 발생
@@ -24,6 +25,14 @@ function webSocket(server: http.Server) {
       socket.emit('chat message', `나: ${data.msg}`);
       // 다른 접속자 화면에 메시지 송출
       socket.broadcast.emit('chat message', `${data.name}: ${data.msg}`);
+      const chatInfo = {
+        roomType: 'main', 
+        username: `${data.name}`,
+        message: `${data.msg}`,
+        time: new Date().getHours().toString(),
+        image: 'no image',
+      };
+      chatService.addChat(chatInfo);
     });
   });
   // socket 연결 종료
