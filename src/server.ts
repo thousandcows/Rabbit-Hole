1d2d216d00bca52bae16221644000d76142d5190
thousandcows <milcamboo@gmail.com>
2022-07-08 21:25:51 +0900

    FEAT ✨: S3 업로드, 다운로드 기능 구현
    1. s3 업로드 기능
    - req에서 file을 받아와 s3 bucket에 file을 업로드한다
    - 업로드 후 promise를 반환한다
    
    2. s3 다운로드 기능
    - fileKey에 해당하는 이미지를 s3로부터 불러온다
    
    # issue2

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import webSocket from './socket';
 import { apiRouter } from './routers/api';
+const fs = require('fs');
+const util = require('util');
+const unlinkFile = util.promisify(fs.unlink);
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const { uploadFile, downloadFile } = require('./s3');
 
 const app = express();
 app.use(cors());
@@ -18,6 +24,18 @@ const { PORT } = process.env;
 app.get('/', (req: any, res: any) => {
   // eslint-disable-next-line no-path-concat, prefer-template
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/images', upload.single('image'), async (req: Request, res: Response) => {
+  const { file } = req;
+  const result = await uploadFile(file);
+  await unlinkFile(file?.path);
+  res.send({ imagePath: `/images/${result.Key}` });
+});
+
+app.get('/images/:key', async (req: Request, res: Response) => {
+  const readStream = await downloadFile(req.params.key);
+  readStream.pipe(res);
 });
 
 app.use('/api', apiRouter);
