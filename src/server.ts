597cacbf1e7a40062151f9bf7cff9cf0b0f26a83
thousandcows <milcamboo@gmail.com>
2022-07-08 21:55:36 +0900

    FEAT ✨: S3 업로드 기능 추가(validation)
    1. 이미지 mime 타입 확인
    - 파일의 mime type이 image인지 확인한다
    
    2. 이미지 크기 확인
    - 파일의 크기가 10MB 이하인지 확인한다
    
    # issue2

--- src/server.ts
+++ src/server.ts
@@ -25,14 +25,31 @@ app.get('/', (req: any, res: any) => {
   // eslint-disable-next-line no-path-concat, prefer-template
   res.sendFile(__dirname + '/index.html');
 });
-
+// 기능 구현 완료 후 routing 작업 예정
+// s3에 파일 이미지 업로드
 app.post('/images', upload.single('image'), async (req: Request, res: Response) => {
+  // 이미지 파일을 요청에서 받아옴
   const { file } = req;
+  // 이미지 파일 mime 타입 확인
+  const fileType = file?.mimetype.split('/')[0];
+  if (fileType !== 'image') {
+    const error = new Error('올바른 이미지 형식이 아닙니다');
+      error.name = 'NotAcceptable';
+      throw error;
+  };
+  // 이미지 크기 확인
+  if (file && file.size >= 1024 * 1024) {
+    const error = new Error('파일 크기는 10MB 이하여야 합니다');
+    error.name = 'NotAcceptable';
+    throw error;
+  };
+  // 이미지 크기 조정 (구현 예정)
+  // 이미지 s3에 업로드
   const result = await uploadFile(file);
   await unlinkFile(file?.path);
   res.send({ imagePath: `/images/${result.Key}` });
 });
-
+// s3에서 파일 이미지 키 다운로드
 app.get('/images/:key', async (req: Request, res: Response) => {
   const readStream = await downloadFile(req.params.key);
   readStream.pipe(res);
