68792017f52c989e67ee097eb28c7ea5896495c5
thousandcows <milcamboo@gmail.com>
2022-07-10 19:47:03 +0900

    feat ✨: 채팅 이미지 전송 기능 추가
    - 이미지가 전송되면 다음 순서로 동작한다
    1. 이미지 타입 확인
    2. 이미지 크기 확인 및 크기 재조정
    3. 이미지 로컬에 임시 저장
    4. 이미지 채팅창 송출 &
    5. s3에 업로드
    6. DB에 저장
    7. 로컬에서 이미지 삭제
    
    #issue: 2

--- src/socket.ts
+++ src/socket.ts
@@ -2,6 +2,20 @@ import * as http from 'http';
 import { Server } from 'socket.io';
 import { chatService } from './services/chat-service';
 
+const fs = require('fs');
+const util = require('util');
+const unlinkFile = util.promisify(fs.unlink);
+const path = require('path');
+const sharp = require('sharp');
+const { uploadFile } = require('./s3');
+interface ChatInfo {
+  roomType: string,
+  username: string,
+  message: string,
+  time: string,
+  image: string,
+}
+
 function webSocket(server: http.Server) {
   const io = new Server(server, {
     cors: {
@@ -9,15 +23,23 @@ function webSocket(server: http.Server) {
       methods: ['GET', 'POST'],
     },
   });
-
   // socket 연결 중
   io.sockets.on('connect', (socket: any) => {
     // 새 접속자 입장
-    socket.on('newUser', (newUser: string) => {
+    socket.on('newUser', async (newUser: string) => {
       // user에게 채팅방에 입장했음을 알림
       socket.emit('updateForNewUser', `${newUser}(나)님이 입장했습니다.`);
       // 전체 접속자에게 user가 채팅방에 입장했음을 알림
       socket.broadcast.emit('updateForEveryone', `${newUser}가 도착했습니다.`);
+      const chatList = await chatService.findAllChats();
+      for (let i = 0; i < 20; i += 1) {
+        const newChat = chatList[i];
+        if (newChat.image === 'no image') {
+          socket.emit('chat message', `${newChat.username}: ${newChat.message}`);
+        } else {
+          socket.emit('image-uploaded', `${newChat.username}: ${newChat.image}`);
+        }
+      }
     });
     // 메시지 전송 발생
     socket.on('chat message', (data: any) => {
@@ -25,7 +47,7 @@ function webSocket(server: http.Server) {
       socket.emit('chat message', `나: ${data.msg}`);
       // 다른 접속자 화면에 메시지 송출
       socket.broadcast.emit('chat message', `${data.name}: ${data.msg}`);
-      const chatInfo = {
+      const chatInfo: ChatInfo = {
         roomType: 'main',
         username: `${data.name}`,
         message: `${data.msg}`,
@@ -33,6 +55,66 @@ function webSocket(server: http.Server) {
         image: 'no image',
       };
       chatService.addChat(chatInfo);
+    });
+    // 이미지 전송 발생
+    socket.on('upload-image', async (message: any) => {
+      // 이미지 타입 확인
+      const fileType = message.type.split('/')[0];
+      if (fileType !== 'image') {
+        const error = new Error('올바른 이미지 형식이 아닙니다');
+        error.name = 'NotAcceptable';
+        throw error;
+      }
+      // 이미지 크기 확인
+      const fileSize = message.data.byteLength;
+      if (fileSize >= 1024 * 1024) {
+        const error = new Error('파일 크기는 10MB 이하여야 합니다');
+        error.name = 'NotAcceptable';
+        throw error;
+      }
+      // 이미지 사이즈 조정
+      const data = await sharp(message.data).metadata();
+      const newFile = await sharp(message.data)
+        .resize({
+          width: Math.trunc(data.width * 0.75),
+          height: Math.trunc(data.height * 0.75),
+          fit: sharp.fit.inside,
+          withoutEnlargement: true
+        })
+        .toBuffer();
+      // 이미지 로컬에 임시 저장
+      const writer = await fs.createWriteStream(path.resolve(__dirname + '/uploads/' + message.name), {
+        encoding: 'base64',
+      });
+      await writer.write(newFile);
+      await writer.end();
+      // 이미지 로컬 저장 후
+      await writer.on('finish', async () => {
+        // 이미지 채팅창 송출
+        socket.emit('image-uploaded', {
+          name: message.name,
+        });
+        socket.broadcast.emit('image-uploaded', {
+          name: message.name,
+        });
+        // 이미지 s3에 업로드
+        const fileToUpload = {
+          path: __dirname + '/uploads/' + message.name,
+          filename: message.name
+        }
+        const uploadResult = await uploadFile(fileToUpload);
+        // 이미지 객체 url DB에 저장
+        const chatInfo: ChatInfo = {
+          roomType: 'main',
+          username: 'username', // 추후 수정 필요함
+          message: 'image', // 추후 수정 필요함
+          time: new Date().getHours().toString(),
+          image: uploadResult.Location,
+        };
+        await chatService.addChat(chatInfo);
+        // 로컬에서 이미지 삭제
+        await unlinkFile(fileToUpload.path);
+      });
     });
   });
   // socket 연결 종료
