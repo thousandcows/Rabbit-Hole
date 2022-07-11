52ce9118de086f6e76d9488fd0f92b1c8e4d072c
chss3339 <chss3339@gmail.com>
2022-07-11 18:34:56 +0900

    feat: login-required 작성

--- src/middlewares/login-required.ts
+++ src/middlewares/login-required.ts
@@ -1,7 +1,7 @@
+import axios from 'axios';
 import { NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 
-function loginRequired(req: Request, res: Response, next: NextFunction) {
+async function loginRequired(req: Request, res: Response, next: NextFunction) {
   // request 헤더로부터 authorization bearer 토큰을 받음.
   // token format 확인
   const wholeToken = req.headers.authorization?.split(' ');
@@ -19,7 +19,7 @@ function loginRequired(req: Request, res: Response, next: NextFunction) {
   }
 
   const userToken = wholeToken[1];
-  // 이 토큰은 jwt 토큰 문자열이거나, 혹은 "null" 문자열이거나, undefined임.
+  // 이 토큰은 github 토큰 문자열이거나, 혹은 "null" 문자열이거나, undefined임.
   // 토큰이 "null" 일 경우, login_required 가 필요한 서비스 사용을 제한함.
   if (!userToken || userToken === 'null') {
     const error = new Error('로그인한 유저만 사용할 수 있는 서비스입니다.');
@@ -29,11 +29,15 @@ function loginRequired(req: Request, res: Response, next: NextFunction) {
 
   // 해당 token 이 정상적인 token인지 확인
   try {
-    const hello = 'hello';
-
+    const userEmail = await axios.get('https://api.github.com/user/emails', {
+      headers: {
+        Accept: 'application/json',
+        Authorization: `token ${userToken}`,
+      },
+    });
+    req.currentGithubEmail = userEmail.data[0].email;
     next();
   } catch (error) {
-    // jwt.verify 함수가 에러를 발생시키는 경우는 토큰이 정상적으로 decode 안되었을 경우임.
     res.status(401).json({
       result: 'Unauthorized',
       reason: '정상적인 토큰이 아닙니다.',
