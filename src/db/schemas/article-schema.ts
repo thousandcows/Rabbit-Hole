25e34ced3be16e90c3c53bfa9abe9e159bdfea73
thousandcows <milcamboo@gmail.com>
2022-07-11 17:29:12 +0900

    feat ✨: article-schema 추가 - article(게시글) schema 생성
    #issue: 3

---
+++ src/db/schemas/article-schema.ts
@@ -0,0 +1,60 @@
+import { Schema } from 'mongoose';
+
+const ArticleSchema = new Schema(
+  {
+    articleType: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    authorId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    likes: [
+      new Schema(
+        {
+          userId: String,
+        },
+        {
+          _id: false,
+        },
+      ),
+    ],
+    views: {
+      type: Number,
+      default: 0,
+    },
+    carrots: {
+      type: Number,
+      default: 0,
+    },
+    tags: [
+      new Schema(
+        {
+          name: String,
+        },
+        {
+          _id: false,
+        },
+      ),
+    ],
+  },
+  {
+    collection: 'articles',
+    timestamps: true,
+  },
+);
+
+export { ArticleSchema };
