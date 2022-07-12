8b657097084e68bbfdb1415e703502d7fec47255
thousandcows <milcamboo@gmail.com>
2022-07-12 14:13:26 +0900

    feat ✨: validation 조건 추가
    1. 게시글 작성 및 수정
    - 글 제목: 최소 1자, 최대 100자
    - 글 내용: 최소 1자, 최대 5000자
    
    2. 게시글 수정 및 삭제
    - userId 와 authorId가 일치하는지 확인
    
    #issue: 3

--- src/utils/validation-article.ts
+++ src/utils/validation-article.ts
@@ -1,4 +1,4 @@
-import { ArticleInfo } from '../db/models/article-model';
+import { ArticleInfo, articleModel } from '../db/models/article-model';
 
 class ArticleValidation {
   createArticle(articleInfo: ArticleInfo) {
@@ -21,21 +21,65 @@ class ArticleValidation {
       const error = new Error('글 제목을 입력해 주세요.');
       error.name = 'BadRequest';
       throw error;
+    } else if (title.length > 100) {
+      const error = new Error('제목을 100자 이내로 입력해주세요.');
+      error.name = 'BadRequest';
+      throw error;
     } else if (!content) {
       const error = new Error('글 내용을 입력해 주세요.');
+      error.name = 'BadRequest';
+      throw error;
+    } else if (content.length > 5000) {
+      const error = new Error('글 내용은 5000자 이내로 입력해 주세요.');
       error.name = 'BadRequest';
       throw error;
     }
   }
 
-  updateArticle(updateInfo: any) {
-    const { title, content } = updateInfo;
+  async updateArticle(userId: string, updateInfo: any) {
+    const { articleId, title, content } = updateInfo;
+    // 유저 === 작성자 확인 필요, unAuthorized
+    const articleInfo = await articleModel.findArticle(articleId);
+    const { authorId } = articleInfo;
+    if (userId !== authorId) {
+      const error = new Error('이 글의 작성자가 아닙니다');
+      error.name = 'BadRequest';
+      throw error;
+    }
+
     if (!title) {
       const error = new Error('글 제목을 입력해 주세요.');
       error.name = 'BadRequest';
       throw error;
+    } else if (title.length < 1) {
+      const error = new Error('제목은 최소 1자 이상 입력해주세요.');
+      error.name = 'BadRequest';
+      throw error;
+    } else if (title.length > 100) {
+      const error = new Error('제목을 100자 이내로 입력해주세요.');
+      error.name = 'BadRequest';
+      throw error;
     } else if (!content) {
       const error = new Error('글 내용을 입력해 주세요.');
+      error.name = 'BadRequest';
+      throw error;
+    } else if (content.length < 1) {
+      const error = new Error('글 내용은 최소 1자 이상 입력해 주세요.');
+      error.name = 'BadRequest';
+      throw error;
+    } else if (content.length > 5000) {
+      const error = new Error('글 내용은 5000자 이내로 입력해 주세요.');
+      error.name = 'BadRequest';
+      throw error;
+    }
+  }
+
+  async deleteArticle(userId: string, articleId: string) {
+    // 유저 === authorId 확인 필요함
+    const articleInfo = await articleModel.findArticle(articleId);
+    const { authorId } = articleInfo;
+    if (userId !== authorId) {
+      const error = new Error('이 글의 작성자가 아닙니다');
       error.name = 'BadRequest';
       throw error;
     }
