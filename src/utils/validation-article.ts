39dcb3a59d540b90a4ebdc33c8427c93953b6195
thousandcows <milcamboo@gmail.com>
2022-07-12 10:44:10 +0900

    feat ✨: article-validation 기능 추가
    1. 새 게시물 생성 기능
    - 새 게시물 생성 요청이 들어오면 schema를 기준으로 validation 진행
    - 게시물 생성 성공 시 결과 반환
    
    2. 게시글 수정
    - 게시글 수정 요청 시 schema를 기준으로 validation 진행
    - 업데이트 성공 시 업데이트된 게시글 반환
    
    #issue: 3

---
+++ src/utils/validation-article.ts
@@ -0,0 +1,47 @@
+import { ArticleInfo } from '../db/models/article-model';
+
+class ArticleValidation {
+  createArticle(articleInfo: ArticleInfo) {
+    const {
+      articleType, author, authorId, title, content
+    } = articleInfo;
+    if (!articleType) {
+      const error = new Error('게시글 타입을 입력해 주세요.');
+      error.name = 'BadRequest';
+      throw error;
+    } else if (!author) {
+      const error = new Error('작성자 이름을 입력해 주세요.');
+      error.name = 'BadRequest';
+      throw error;
+    } else if (!authorId) {
+      const error = new Error('작성자 아이디를 입력해 주세요.');
+      error.name = 'BadRequest';
+      throw error;
+    } else if (!title) {
+      const error = new Error('글 제목을 입력해 주세요.');
+      error.name = 'BadRequest';
+      throw error;
+    } else if (!content) {
+      const error = new Error('글 내용을 입력해 주세요.');
+      error.name = 'BadRequest';
+      throw error;
+    }
+  }
+
+  updateArticle(updateInfo: any) {
+    const { title, content } = updateInfo;
+    if (!title) {
+      const error = new Error('글 제목을 입력해 주세요.');
+      error.name = 'BadRequest';
+      throw error;
+    } else if (!content) {
+      const error = new Error('글 내용을 입력해 주세요.');
+      error.name = 'BadRequest';
+      throw error;
+    }
+  }
+}
+const articleValidation = new ArticleValidation();
+
+export { articleValidation }
+
