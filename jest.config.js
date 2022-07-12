ade19ca8cdb62238ab9930987525520ac057eee5
chss3339 <chss3339@gmail.com>
2022-07-12 22:16:07 +0900

    fix: jest.config.js 수정

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
-  moduleNameMapper: {
-    '@routes/(.*)$': '<rootDir>/src/routers/$1',
-    '@services/(.*)$': '<rootDir>/src/services/$1',
-    '@models/(.*)$': '<rootDir>/src/models/$1',
-  },
+  testEnvironment: 'node', // 테스트 환경 'node' 환경을 사용한다 알려줌
+  testMatch: ['**/tests/*.test.(ts|tsx)'], // js 파일은 dist에서도 감지가 될 수 있으니 폴더를 조정해서 test이 있는 위치로 잡아준다.
 };
