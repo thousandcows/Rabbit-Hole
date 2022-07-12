548fa2bb7602f2a3e4da033551a611286e96614c
thousandcows <milcamboo@gmail.com>
2022-07-12 10:44:41 +0900

    feat ✨: article-router 추가
    #issue: 3

--- src/routers/api.ts
+++ src/routers/api.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { userRouter, chatRouter } from './api/index';
+import { userRouter, chatRouter, articleRouter } from './api/index';
 
 const apiRouter = Router();
 apiRouter.use('/users', userRouter);
 apiRouter.use('/chats', chatRouter);
+apiRouter.use('/articles', articleRouter);
 
 export { apiRouter };
--- src/routers/api/index.ts
+++ src/routers/api/index.ts
@@ -1,2 +1,3 @@
 export * from './user-router';
 export * from './chat-router';
+export * from './article-router';
