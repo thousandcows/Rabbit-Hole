2a29efb9dc862ee1f70900e1aa6b0a5eeb7cfead
thousandcows <milcamboo@gmail.com>
2022-07-13 21:42:35 +0900

    feat ✨:  adminRequired 미들웨어 추가
    - 사용자의 'role'을 확인해 관리자인지 검증하는 미들웨어
    
    #issue 22

---
+++ src/middlewares/admin-required.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import { userService } from '../services/user-service';
+
+async function adminRequired(req: Request, res: Response, next: NextFunction) {
+  const adminId = req.currentUserId;
+  const adminInfo = await userService.getUserById(adminId);
+
+  if (adminInfo.role === 'admin') {
+    next();
+  } else {
+    const error = new Error('관리자 권한이 없습니다.');
+    error.name = 'Forbidden';
+    throw error;
+  }
+}
+
+export { adminRequired };
