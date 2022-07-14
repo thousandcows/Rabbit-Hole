d9a43746c4ed88acd21358bbb80f422a19388987
chss3339 <chss3339@gmail.com>
2022-07-14 17:23:36 +0900

    fix: search api 변경

--- src/routers/api/search-router.ts
+++ src/routers/api/search-router.ts
@@ -6,7 +6,7 @@ import { articleService, projectService } from '../../services';
 const searchRouter = Router();
 
 // 1. 게시글 검색
-searchRouter.get('/article', async (req: Request, res: Response, next: NextFunction) => {
+searchRouter.get('/articles', async (req: Request, res: Response, next: NextFunction) => {
   try {
     if (req.query.author && req.query.type) {
       const { author, type } = req.query;
