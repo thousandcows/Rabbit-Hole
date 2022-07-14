e5646f1bdbfd2894244ca5538b76bb7bc44a1000
thousandcows <milcamboo@gmail.com>
2022-07-15 01:42:24 +0900

    feat ✨:  multer-s3 이미지 validation 기능 - mimetype 확인: jpeg, jpg, png, gif - 10MB 이하의 파일만 업로드

--- src/utils/multer-s3.ts
+++ src/utils/multer-s3.ts
@@ -1,7 +1,9 @@
 require('dotenv').config();
+import { Request } from 'express';
+
 const S3 = require('aws-sdk/clients/s3');
 const multer = require('multer');
-const multerS3 = require('multer-s3-transform');
+const multerS3 = require('multer-s3');
 
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
@@ -15,12 +17,29 @@ const s3 = new S3({
   secretAccessKey,
 });
 
-const upload = multer({
-  storage: multerS3({
-    s3,
-    bucket: bucketName,
-    acl: 'public-read',
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-  }),
+const storage = multerS3({
+  s3,
+  bucket: bucketName,
+  acl: 'public-read',
+  contentType: multerS3.AUTO_CONTENT_TYPE,
 });
-module.exports = upload;
+
+const fileFilter = (req: Request, file: any, cb: any) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/gif' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    const error = new Error('지원하는 이미지 타입이 아닙니다');
+    error.name = 'Forbidden';
+    throw error;
+   }
+};
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1024 * 1024,
+  },
+  fileFilter,
+});
+
+export { upload };
