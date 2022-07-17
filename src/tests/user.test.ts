7fd5bcd019fcbf77806b44db2f57d1108398e676
chss3339 <chss3339@gmail.com>
2022-07-18 00:35:07 +0900

    feat: user api test 완료

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -33,8 +33,14 @@ const my = {
   githubProfileUrl: '프로필',
 };
 
+const updateMock = {
+  track: 'AI트랙',
+  trackCardinalNumber: 5,
+};
+
 beforeAll(async () => {
   db.connect();
+  // 테스트 시작 전 테스트db에 유저정보 저장
   const result = await request(app).post('/api/users/register').field(my).attach('authImage', path.join(__dirname, '/garbage.png'));
   token = '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';
 });
@@ -74,5 +80,33 @@ describe('Report Function', () => {
     jest.setTimeout(30000);
     const res = await request(app).post('/api/users/register').field(my).attach('authImage', path.join(__dirname, '/garbage.png'));
     expect(res.statusCode).toBe(409);
+  });
+  test('마이페이지 - 게시글 조회 성공', async () => {
+    jest.setTimeout(30000);
+    const myinfo = await request(app).get('/api/users/mypage').set('Authorization', `Bearer ${token}`);
+    const res = await request(app).get(`/api/users/${myinfo.body._id}/articles`).set('Authorization', `Bearer ${token}`);
+    expect(res.statusCode).toBe(200);
+  });
+  test('마이페이지 - 프로젝트 조회 성공', async () => {
+    jest.setTimeout(30000);
+    const myinfo = await request(app).get('/api/users/mypage').set('Authorization', `Bearer ${token}`);
+    const res = await request(app).get(`/api/users/${myinfo.body._id}/projects`).set('Authorization', `Bearer ${token}`);
+    expect(res.statusCode).toBe(200);
+  });
+  test('회원정보 수정', async () => {
+    jest.setTimeout(30000);
+    const res = await request(app).put('/api/users').send(updateMock).set('Authorization', `Bearer ${token}`);
+    expect(res.body.track).toBe('AI트랙');
+    expect(res.body.trackCardinalNumber).toBe(5);
+  });
+  test('이메일로 회원 조회', async () => {
+    jest.setTimeout(30000);
+    const res = await request(app).get('/api/users/chss3339@gmail.com').set('Authorization', `Bearer ${token}`);
+    expect(res.statusCode).toBe(200);
+  });
+  test('회원탈퇴', async () => {
+    jest.setTimeout(30000);
+    const res = await request(app).delete('/api/users').set('Authorization', `Bearer ${token}`);
+    expect(res.statusCode).toBe(200);
   });
 });
