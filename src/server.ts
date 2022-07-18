8209889f3366fd4471e8f3867cd26f66dacd73a4
thousandcows <milcamboo@gmail.com>
2022-07-18 11:08:06 +0900

    style 🎨:  테스트 고려하여 코드 수정

--- src/server.ts
+++ src/server.ts
@@ -25,20 +25,16 @@ app.use(morgan(':method :url :status :response-time ms - :res[content-length] :d
 app.use('/api', apiRouter);
 app.use(errorHandler);
 if (process.env.NODE_ENV !== 'test') {
+  // 서버 연결
   const server = app.listen(PORT, () => console.log(`server is running ${PORT}`));
   webSocket(server);
+  // redis 클라우드 연결
+  client.connect();
+  // toad-schedular: Redis => DB 정기 업데이트
+  const scheduler = new ToadScheduler();
+  const task = new Task('updateDatabase', async () => { await updateDatabase(); });
+  const job = new SimpleIntervalJob({ seconds: 10 }, task);
+  scheduler.addSimpleIntervalJob(job);
 }
-
-// redis-connect
-client.connect();
-
-// toad-schedular: Redis => DB 정기 업데이트
-const scheduler = new ToadScheduler();
-const task = new Task('updateDatabase', async () => { await updateDatabase(); });
-const job = new SimpleIntervalJob({ seconds: 5 }, task);
-
-scheduler.addSimpleIntervalJob(job);
-
-// socket
 
 export { app };
