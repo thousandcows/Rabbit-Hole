0b2e1e9d19c99da8a99bcc9072f2aa1012b1c5cd
thousandcows <milcamboo@gmail.com>
2022-07-19 15:01:54 +0900

    feat✨: 채팅 테스트 보완 - 채팅 내용 삽입 후 내용 조회하는 테스트 실행

--- src/tests/chat.test.ts
+++ src/tests/chat.test.ts
@@ -6,9 +6,51 @@ import path from 'path';
 import request from 'supertest';
 import * as db from './utils/db';
 import { app } from '../server';
+import { Types, Document } from 'mongoose';
+import { chatService } from '../services';
+
+let token: string;
+let userId: string;
+
+const userInfo = {
+  name: 'jest1',
+  track: 'SW트랙',
+  trackCardinalNumber: 1,
+  position: '프론트엔드',
+  githubAvatar: '아바타',
+  githubEmail: 'chss3339@gmail.com',
+  githubProfileUrl: '프로필',
+};
+
+const chatMock = {
+  senderId: 'milmil',
+  profile: 'profile string',
+  name: 'cow',
+  track: 'SW트랙',
+  trackCardinalNumber: 2,
+  roomType: 'main',
+  chat: 'I like socket.io',
+  time: '20:33',
+  image: 'no image',
+}
+
+interface ChatData extends Document<Types.ObjectId> {
+  senderId: string,
+  profile: string,
+  name: string,
+  track: string,
+  trackCardinalNumber: number,
+  chat: string,
+  time?: string,
+  image: string,
+  roomType: string,
+}
 
 beforeAll(async () => {
   db.connect();
+  await request(app).post('/api/users/register').field(userInfo).attach('authImage', path.join(__dirname, '/garbage.png'));
+  token = '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';
+  await chatService.addChat(chatMock);
 });
 afterAll(() => {
   db.close();
@@ -17,8 +59,8 @@ afterAll(() => {
 describe('article-router 게시판 API 테스트', () => {
   test('채팅 내역 조회', async () => {
     jest.setTimeout(30000);
-    const res = await request(app).get('/api/chats').query({ page: 1, perPage: 10 });
+    const res = await request(app).get('/api/chats').query({ page: 1, perPage: 10 }).set('Authorization', `Bearer ${token}`);
     expect(res.statusCode).toBe(200);
-    expect(res.body.totalPage).toBeGreaterThanOrEqual(0);
+    expect(res.body.totalPage).toBeGreaterThanOrEqual(1);
   });
 });
