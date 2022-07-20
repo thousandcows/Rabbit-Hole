99fff92ec34bb95c8e705e2c4b9ef455c6f75425
chss3339 <chss3339@gmail.com>
2022-07-20 18:39:27 +0900

    feat: 유저아이디 회원조회

--- src/routers/api/user-router.ts
+++ src/routers/api/user-router.ts
@@ -91,10 +91,9 @@ userRouter.get('/:githubEmail', loginRequired, async (req: any, res: Response, n
 });
 
 // userId로 회원 조회
-userRouter.get('/:userId', loginRequired, async (req: any, res: Response, next: NextFunction) => {
+userRouter.get('/profile/:userId', loginRequired, async (req: any, res: Response, next: NextFunction) => {
   try {
     const { userId } = req.params;
-
     const userData = await userService.getUserById(userId);
 
     res.status(200).json(userData);
